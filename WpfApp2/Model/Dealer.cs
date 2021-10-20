82e2ffdbeadc07275bc7b6884654862a745bcb1f
momo9393 <i.can.2028.tcc@gmail.com>
2021-10-20 19:43:23 +0200

    Model Classes added and adjustment to many parts of the GUI

---
+++ WpfApp2/Model/Dealer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Model
+{
+    class Dealer
+    {
+        #region fields
+        private string _name;
+        private int? _cardTotal;
+        private List<string> _card;
+        private string _hiddenCard;
+        private int _hiddenCardTotal;
+        #endregion
+
+        #region properties
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+               //OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int? CardTotal
+        {
+            get => _cardTotal;
+            set
+            {
+                _cardTotal = value;
+               //OnPropertyChanged(nameof(CardTotal));
+            }
+        }
+
+        public List<string> Card
+        {
+            get => _card;
+            set
+            {
+                _card = value;
+               //OnPropertyChanged(nameof(Card));
+            }
+        }
+
+        public string HiddenCard
+        {
+            get => _hiddenCard;
+            set
+            {
+                _hiddenCard = value;
+               //OnPropertyChanged(nameof(Player));
+            }
+        }
+
+        public int HiddenCardTotal
+        {
+            get => _hiddenCardTotal;
+            set
+            {
+                _hiddenCardTotal = value;
+               //OnPropertyChanged(nameof(HiddenCardTotal));
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Dealer(string name)
+        {
+            _name = name;
+            this.Card = new List<string>();
+        }
+        #endregion
+    }
+}
---
+++ WpfApp2/Model/Player.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Model
+{
+    class Player
+    {
+        #region fields
+        private string _name;
+        private int _bankRoll;
+        private int _totalBet;
+        private int _totalWinnings;
+        private int? _cardTotal;
+        private List<string> _card;
+        private int _betAmount;
+        #endregion
+
+        #region properties
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+               //OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int BankRoll
+        {
+            get => _bankRoll;
+            set
+            {
+                _bankRoll = value;
+               //OnPropertyChanged(nameof(BankRoll));
+            }
+        }
+
+        //[JsonIgnore]
+        public int TotalBet
+        {
+            get => _totalBet;
+            set
+            {
+                _totalBet = value;
+               //OnPropertyChanged(nameof(TotalBet));
+            }
+        }
+
+        //[JsonIgnore]
+        public int TotalWinnings
+        {
+            get => _totalWinnings;
+            set
+            {
+                _totalWinnings = value;
+               //OnPropertyChanged(nameof(TotalWinnings));
+            }
+        }
+
+        //[JsonIgnore]
+        public int? CardTotal
+        {
+            get => _cardTotal;
+            set
+            {
+                _cardTotal = value;
+               //OnPropertyChanged(nameof(CardTotal));
+            }
+        }
+
+
+        //[JsonIgnore]
+        public List<string> Card
+        {
+            get => _card;
+            set
+            {
+                _card = value;
+               //OnPropertyChanged(nameof(Card));
+            }
+        }
+
+        //[JsonIgnore]
+        public int BetAmount
+        {
+            get => _betAmount;
+            set
+            {
+                _betAmount = value;
+               //OnPropertyChanged(nameof(BetAmount));
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        public Player(string name)
+        {
+            _name = name;
+            Card = new List<string>();
+        }
+        #endregion
+
+    }
+}
